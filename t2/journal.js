c3beaf0010d535ce261679540f2ea25ee2efa150
kathy tran <ktran20@fordham.edu>
2020-12-12 10:33:30 -0500

    Update journal.js

--- t2/journal.js
+++ t2/journal.js
@@ -207,9 +207,7 @@ function createCookie(name, value, days) {
     date.setTime(date.getTime()+(days*24*60*60*1000));
     var expires = "; expires="+date.toGMTString();
   }
-  else {
-    var expires = "";
-  }
+  else var expires = "";
   document.cookie = name+"="+value+expires+"; path=/";
 }
 
@@ -219,17 +217,12 @@ function readCookie(name) {
   var ca = document.cookie.split(';');
   for(var i=0;i < ca.length;i++) {
     var c = ca[i];
-    while (c.charAt(0)==' '){
-      c = c.substring(1,c.length);
-    }
-    if (c.indexOf(nameEQ) == 0){
-      return c.substring(nameEQ.length,c.length);
-    }
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
   }
   return null;
 }
 
-// delete cookies
 function eraseCookie(name) {
     createCookie(name,"",-1);
 }
